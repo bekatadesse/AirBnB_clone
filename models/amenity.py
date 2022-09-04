33528eb98291d4e40b76ffcdecb086cc4c007f87
bekatadesse <bekatadesse5@gmail.com>
2022-09-04 11:53:42 +0300

    added amenity model

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Contains Class that defines the Amenity model.
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represent an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
